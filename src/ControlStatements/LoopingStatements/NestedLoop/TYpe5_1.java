ea11c2a6162227c15566f6a45a5095027470a32e
Mshaveta <Shaveta.malhotra47@gmail.com>
2022-07-08 20:26:17 +0530

    Pattern_4&5

---
+++ src/ControlStatements/LoopingStatements/NestedLoop/TYpe5_1.java
@@ -0,0 +1,42 @@
+package operators.ControlStatements.LoopingStatements.NestedLoop;
+/*
+	1	2	3
+1	*
+2	*	*
+3	*	*	*
+4	*	*
+5	*
+
+
+ */
+public class TYpe5_1 {
+    public static void main(String[] args) {
+
+       int a=10;
+       int b =20;
+        boolean x=false;
+        System.out.println( (a>b) ? x=true : x );
+
+
+
+
+//        int n=5;
+//        int half=(n+1)/2;//3
+//        System.out.println(half);
+//        int st=0;
+//        for(int row=1;row<=n;row++){
+//            //1<=3, 2<=3,3<=3, 4<=3, 5<=3
+//            if(row<=half){
+//                st = row;//st=1,2,3
+//            }else{
+//                st--;//3->2->1
+//            }
+//
+//            for(int star=1;star<= st ;star++){
+//                System.out.print("* ");
+//            }
+//
+//            System.out.println();
+//        }
+    }
+}
