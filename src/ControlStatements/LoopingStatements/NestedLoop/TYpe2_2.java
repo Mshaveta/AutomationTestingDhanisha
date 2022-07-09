0b5ca534cc6a0633be07bfddfb94694989b5f13f
Mshaveta <Shaveta.malhotra47@gmail.com>
2022-07-09 09:07:05 +0530

    Type6Programs

---
+++ src/ControlStatements/LoopingStatements/NestedLoop/TYpe2_2.java
@@ -0,0 +1,16 @@
+package operators.ControlStatements.LoopingStatements.NestedLoop;
+
+public class TYpe2_2 {
+    public static void main(String[] args) {
+        int n=5;
+        char ch = 'a';
+        for(int i=1;i<=n;i++){
+
+            for(int j=1;j<=i;j++){
+                System.out.print(ch);
+            }
+            ch++;
+            System.out.println();
+        }
+    }
+}
--- src/ControlStatements/LoopingStatements/NestedLoop/TYpe5_1.java
+++ src/ControlStatements/LoopingStatements/NestedLoop/TYpe5_1.java
@@ -12,31 +12,22 @@ package operators.ControlStatements.LoopingStatements.NestedLoop;
 public class TYpe5_1 {
     public static void main(String[] args) {
 
-       int a=10;
-       int b =20;
-        boolean x=false;
-        System.out.println( (a>b) ? x=true : x );
+        int n=5;
+        int half=(n+1)/2;//3
+        System.out.println(half);
+        int st=0;
+        for(int row=1;row<=n;row++){
+            //1<=3, 2<=3,3<=3, 4<=3, 5<=3
+            if(row<=half){
+                st = row;//st=1,2,3
+            }else{
+                st--;//3->2->1
+            }
 
-
-
-
-//        int n=5;
-//        int half=(n+1)/2;//3
-//        System.out.println(half);
-//        int st=0;
-//        for(int row=1;row<=n;row++){
-//            //1<=3, 2<=3,3<=3, 4<=3, 5<=3
-//            if(row<=half){
-//                st = row;//st=1,2,3
-//            }else{
-//                st--;//3->2->1
-//            }
-//
-//            for(int star=1;star<= st ;star++){
-//                System.out.print("* ");
-//            }
-//
-//            System.out.println();
-//        }
+            for(int star=1;star<= st ;star++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
